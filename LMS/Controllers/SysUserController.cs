0de3eb03dd815790e21d7912bd5730eeed6717de
yiyun <yiyungent@gmail.com>
2019-02-28 09:37:34 +0800

    创建页面关闭按钮完成

---
+++ LMS/Controllers/SysUserController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LMS.Models;
+
+namespace LMS.Controllers
+{
+    public class SysUserController : Controller
+    {
+        #region 列表
+        public ActionResult Index()
+        {
+            IList<SysUser> lst = new List<SysUser>();
+            lst.Add(new SysUser()
+            {
+                ID = 1,
+                Name = "刘备",
+                LoginAccount = "lb",
+                Password = "123456",
+                Status = 0
+            });
+            lst.Add(new SysUser()
+            {
+                ID = 2,
+                Name = "关羽",
+                LoginAccount = "gy",
+                Password = "123456",
+                Status = 0
+            });
+            lst.Add(new SysUser()
+            {
+                ID = 3,
+                Name = "张飞",
+                LoginAccount = "zf",
+                Password = "12345",
+                Status = 0
+            });
+            lst.Add(new SysUser()
+            {
+                ID = 4,
+                Name = "赵云",
+                LoginAccount = "zy",
+                Password = "123456",
+                Status = 0
+            });
+            lst.Add(new SysUser()
+            {
+                ID = 5,
+                Name = "马超",
+                LoginAccount = "mc",
+                Password = "123456",
+                Status = 0
+            });
+
+            return View(lst); // 强类型视图
+        }
+        #endregion
+
+        #region 新增
+        public ActionResult Create()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 修改
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 删除
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 明细
+        public ActionResult Details()
+        {
+            return View();
+        }
+        #endregion
+
+        #region 登录
+        public ActionResult Login()
+        {
+            return View();
+        }
+        #endregion
+    }
+}
---
+++ LMS/Models/SysUser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace LMS.Models
+{
+    /// <summary>
+    /// 实体类：用户
+    /// </summary>
+    public class SysUser
+    {
+        /// <summary>
+        /// 用户编号
+        /// </summary>
+        [Display(Name = "用户编号")]
+        public int ID { get; set; }
+
+        /// <summary>
+        /// 用户名
+        /// </summary>
+        [Display(Name = "用户名")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 登录账号
+        /// </summary>
+        [Display(Name = "登录账号")]
+        public string LoginAccount { get; set; }
+
+        /// <summary>
+        /// 密码
+        /// </summary>
+        [Display(Name = "密码")]
+        public string Password { get; set; }
+
+        /// <summary>
+        /// 状态
+        ///     0: 正常
+        ///     1: 禁用
+        /// </summary>
+        [Display(Name = "状态")]
+        public int Status { get; set; }
+    }
+}
