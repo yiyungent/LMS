088a630501abb8b2bf4db5fc14f7908dd1eda5de
yiyun <yiyungent@gmail.com>
2019-03-01 12:08:30 +0800

    global.asax,web.config

--- Webs/Global.asax.cs
+++ Webs/Global.asax.cs
@@ -6,14 +6,34 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Routing;
 
+using Castle.ActiveRecord;
+using Castle.ActiveRecord.Framework;
+using Core;
+using Domain;
+using NHibernate;
+
 namespace Webs
 {
     // 注意: 有关启用 IIS6 或 IIS7 经典模式的说明，
     // 请访问 http://go.microsoft.com/?LinkId=9394801
     public class MvcApplication : System.Web.HttpApplication
     {
+        private Container container;
         protected void Application_Start()
         {
+            
+            // 容器初始化
+            try
+            {
+                // 获取 web.config 配置
+                IConfigurationSource source = (IConfigurationSource)System.Configuration.ConfigurationManager.GetSection("ActiveRecord");
+                ActiveRecordStarter.Initialize(typeof(SysUser).Assembly, source);
+                container = Container.Instance;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
