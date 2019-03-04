89a783dcd61ec57a5d728a92b3422d65be84ca91
yiyun <yiyungent@gmail.com>
2019-03-04 10:48:45 +0800

    InitDB.cshtml 准备

---
+++ Domain/SysMenu.cs
@@ -0,0 +1,54 @@
+﻿using Castle.ActiveRecord;
+using System.ComponentModel.DataAnnotations;
+
+namespace Domain
+{
+    /// <summary>
+    /// 实体类: 系统菜单
+    /// </summary>
+    [ActiveRecord]
+    public class SysMenu : BaseEntity<SysMenu>
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        [Display(Name = "名称")]
+        [Property]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 类名
+        /// </summary>
+        [Display(Name = "类名")]
+        [Property]
+        public string ClassName { get; set; }
+
+        /// <summary>
+        /// 控制器
+        /// </summary>
+        [Display(Name = "控制器")]
+        [Property]
+        public string ControllerName { get; set; }
+
+        /// <summary>
+        /// 动作
+        /// </summary>
+        [Display(Name = "动作")]
+        [Property]
+        public string ActionName { get; set; }
+
+        /// <summary>
+        /// 排序码
+        /// </summary>
+        [Display(Name = "排序码")]
+        [Property]
+        public int SortCode { get; set; }
+
+        /// <summary>
+        /// 上级菜单编号
+        /// </summary>
+        [Display(Name = "上级菜单编号")]
+        [Property]
+        public int ParentId { get; set; }
+    }
+}
---
+++ Webs/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Webs.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        #region 初始化数据库
+        public ActionResult InitDB()
+        {
+            return View();
+        }
+        #endregion
+
+    }
+}
