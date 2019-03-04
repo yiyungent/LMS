2ac9e9b15ef6c1a9d83df7416384eac947bbe34b
yiyun <yiyungent@gmail.com>
2019-03-04 13:10:03 +0800

    数据库安装程序

--- Webs/Controllers/HomeController.cs
+++ Webs/Controllers/HomeController.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using Castle.ActiveRecord;
+
 namespace Webs.Controllers
 {
     public class HomeController : Controller
@@ -20,6 +22,26 @@ namespace Webs.Controllers
         public ActionResult InitDB()
         {
             return View();
+        }
+        public ActionResult StartInitDB()
+        {
+            CreateDB();
+            return View("InitDB");
+        }
+
+        private void CreateDB()
+        {
+            try
+            {
+                Response.Write("开始创建数据库结构<br/>");
+                ActiveRecordStarter.CreateSchema();
+                Response.Write("......成功<br/>");
+            }
+            catch (Exception ex)
+            {
+                //Response.Write("......失败！原因：" + ex.Message + "<br/>");
+                Response.Write(string.Format("......失败！原因： {0}<br/>", ex.Message));
+            }
         }
         #endregion
 
