b16d083b46dfa81cd626eaf1c34c95354195bf95
yiyun <yiyungent@gmail.com>
2019-03-08 08:30:59 +0800

    add InitClazz

--- Webs/Controllers/HomeController.cs
+++ Webs/Controllers/HomeController.cs
@@ -36,7 +36,58 @@ namespace Webs.Controllers
         {
             CreateSchema();
             InitUser();
+            InitClazz();
+            InitStudent();
         }
+
+        #region 初始化学生
+        private void InitStudent()
+        {
+            string[] names = { "张三", "李四", "王五" };
+            int[] sexs = { 0, 1, 0 };
+            for (int i = 0; i < names.Length; i++)
+            {
+                Clazz clazz = Container.Instance.Resolve<ClazzService>().GetEntity(i + 1);
+
+                Container.Instance.Resolve<StudentService>().Create(new Student()
+                {
+                    Name = names[i],
+                    Sex = sexs[i],
+                    #region 左补0其它方法
+                    // 左补0, 这里最终2位
+                    // 1.
+                    //StudyNumber = "1700103" + i.ToString().PadLeft(2, '0'),
+                    // 2.
+                    //StudyNumber = "1700103" + string.Format("{0:d2}", i),
+                    // 3.
+                    //StudyNumber = "1700103" + i.ToString("00"), 
+                    #endregion
+                    // 4.
+                    StudyNumber = "1700103" + i.ToString("D2"),
+
+                    Mobile = "15098443993",
+                    Clazz = clazz
+                });
+            }
+        } 
+        #endregion
+
+        #region 初始化班级
+        private void InitClazz()
+        {
+            string[] names = { };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                IList<Student> studentList = Container.Instance.Resolve<StudentService>().GetAll().Where(s => s.Clazz.ID == i + 1).ToList();
+                Container.Instance.Resolve<ClazzService>().Create(new Clazz()
+                {
+                    Name = names[i],
+                    StudentList = studentList
+                });
+            }
+        } 
+        #endregion
 
         #region 初始化用户
         private void InitUser()
