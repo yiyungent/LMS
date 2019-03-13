de29401ab081fb404e54c8d640708e462b066e5b
yiyun <yiyungent@gmail.com>
2019-03-14 02:03:14 +0800

    add Student Create

--- Webs/Controllers/StudentController.cs
+++ Webs/Controllers/StudentController.cs
@@ -7,6 +7,7 @@ using Domain;
 using Service;
 using Core;
 using NHibernate.Criterion;
+using Comm;
 
 namespace Webs.Controllers
 {
@@ -34,6 +35,76 @@ namespace Webs.Controllers
                 //  非 pjax -- 完整视图
                 return View(data.ToList());
             }
+        }
+        #endregion
+
+        #region 新增
+        [HttpGet]
+        public ActionResult Create()
+        {
+            // 1.准备实体
+            Student mo = new Student();
+            // 2.返回前预处理
+            ViewBag.rblSex = InitRBLForSex(0);
+
+            IList<Clazz> list = Container.Instance.Resolve<ClazzService>().GetAll();
+            var clazzsIDAndName = from m in list
+                                  select new { m.ID, m.Name };
+            Dictionary<int, string> idAndNameDic = new Dictionary<int, string>();
+            foreach (int id in clazzsIDAndName.Select(m => m.ID))
+            {
+                idAndNameDic.Add(id, clazzsIDAndName.Where(m => m.ID == id).First().Name);
+            }
+            ViewBag.ddlClazz = idAndNameDic;
+
+            // 3.返回视图
+            return View(mo);
+        }
+
+        [HttpPost]
+        public string Create(Student mo)
+        {
+            try
+            {
+                mo.Sex = mo.Sex == 0 || mo.Sex == 1 ? mo.Sex : 0;
+                int clazzId;
+                if (Request["Clazz"] != null && int.TryParse(Request["Clazz"], out  clazzId))
+                {
+                    mo.Clazz = Container.Instance.Resolve<ClazzService>().GetEntity(clazzId);
+                }
+
+                Container.Instance.Resolve<StudentService>().Create(mo);
+
+                return "ok";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+        #endregion
+
+        #region 辅助方法
+        /// <summary>
+        /// 初始化单选备选项-性别
+        /// </summary>
+        private IList<SelectListItem> InitRBLForSex(int selectedValue)
+        {
+            IList<SelectListItem> ret = new List<SelectListItem>();
+            ret.Add(new SelectListItem()
+            {
+                Text = "男",
+                Value = "0",
+                Selected = (selectedValue == 0),
+            });
+            ret.Add(new SelectListItem()
+            {
+                Text = "女",
+                Value = "1",
+                Selected = (selectedValue == 1),
+            });
+
+            return ret;
         }
         #endregion
     }
---
+++ Webs/Extensions/HtmlHelperExtRBL.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using System.Text;
+
+namespace Webs.Extensions
+{
+    public static class HtmlHelperExtRBL
+    {
+        /// <summary>
+        /// eg: <input type="radio" name="sex" value="0" title="男" checked="checked">
+        /// </summary>
+        public static MvcHtmlString RadioButtonList(this HtmlHelper helper, string name, IEnumerable<SelectListItem> selectList, bool useTitle, object htmlAttributes)
+        {
+            int index = 0;
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (SelectListItem selectItem in selectList)
+            {
+                IDictionary<string, object> newHtmlAttributes = HtmlHelper.AnonymousObjectToHtmlAttributes(htmlAttributes);
+                index++;
+                newHtmlAttributes.Add("id", name + "_" + index.ToString());
+                newHtmlAttributes.Add("name", name);
+                newHtmlAttributes.Add("type", "radio");
+                newHtmlAttributes.Add("value", selectItem.Value);
+                newHtmlAttributes.Add("title", selectItem.Text);
+                if (selectItem.Selected)
+                {
+                    newHtmlAttributes.Add("checked", "checked");
+                }
+                TagBuilder tagBuilder = new TagBuilder("input");
+                tagBuilder.MergeAttributes<string, object>(newHtmlAttributes);
+                string inputAllHtml = tagBuilder.ToString(TagRenderMode.SelfClosing);
+                stringBuilder.Append(inputAllHtml);
+            }
+            return MvcHtmlString.Create(stringBuilder.ToString());
+        }
+
+        public static MvcHtmlString RadioButtonList(this HtmlHelper helper, string name, IEnumerable<SelectListItem> selectList, bool useTitle)
+        {
+            return RadioButtonList(helper, name, selectList, useTitle, new { });
+        }
+    }
+}
