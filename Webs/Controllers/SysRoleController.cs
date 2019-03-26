34d8641bceebe9f3bfc86a145f4c85bd650be050
yiyun <yiyungent@gmail.com>
2019-03-26 10:57:24 +0800

    角色权限树

--- Webs/Controllers/SysRoleController.cs
+++ Webs/Controllers/SysRoleController.cs
@@ -9,6 +9,7 @@ using System.Web.Mvc;
 using NHibernate.Criterion;
 using Comm;
 using PagedList;
+using System.Text;
 
 namespace Webs.Controllers
 {
@@ -51,6 +52,7 @@ namespace Webs.Controllers
             SysRole mo = new SysRole();
             // 2.返回前预处理
             ViewBag.rblStatus = InitRBLForStatus(0);
+            ViewBag.AuthIds = "";
             // 3.返回视图
             return View(mo);
         }
@@ -82,6 +84,65 @@ namespace Webs.Controllers
                 // 4.保存失败返回异常
                 return ex.Message;
             }
+        }
+
+        public void GenXmlForMenuTree()
+        {
+            StringBuilder sb = new StringBuilder();
+            // 1.获取全部菜单与操作
+            IList<SysMenu> allMenu = Container.Instance.Resolve<SysMenuService>().GetAll();
+            IList<SysFunction> allFunction = Container.Instance.Resolve<SysFunctionService>().GetAll();
+            // 2.写XML的头
+            sb.Append("<?xml version='1.0' encoding='utf-8'?>");
+            sb.Append("<tree id='0'>");
+            // 3.写 XML 数据区
+            // 3.1 确定一级菜单
+            var first = from m in allMenu
+                        where m.ParentMenu == null
+                        orderby m.SortCode
+                        select m;
+            // 3.2 关于一级菜单循环，递归添加菜单及其子女到sb
+            foreach (var item in first)
+            {
+                AddSelfAndChildren(item, allMenu, allFunction, sb);
+            }
+            // 4.写 XML 的尾
+            sb.Append("</tree>");
+
+            // 5.输出文件流
+            Response.ContentType = "text/xml;charset=utf-8";
+            Response.Write(sb.ToString());
+            Response.Flush();
+            Response.End();
+        }
+
+        private void AddSelfAndChildren(SysMenu self, IList<SysMenu> allMenu, IList<SysFunction> allFunction, StringBuilder sb)
+        {
+            // 1.添加自己
+            // 1.1添加 Menu
+            sb.Append(string.Format("<item id='m{0}' text='{1}' im0='{2}' im1='{2}' im2='{2}' open='1'>", self.ID, self.Name, "book_titel.gif"));
+            // 1.2添加 Function
+            var find = from m in allFunction
+                       where m.SysMenu != null && m.SysMenu.ID == self.ID
+                       select m;
+            foreach (var item in find)
+            {
+                sb.Append(string.Format("<item id='f{0}' text='{1}' im0='{2}' im1='{2}' im2='{2}' open='1' />", item.ID, item.Name, "book_titel.gif"));
+            }
+            // 2.递归添加子女
+            // 2.1筛选子女
+            //var children = from m in allMenu
+            //               where m.ParentMenu != null && m.ParentMenu.ID == self.ID
+            //               orderby m.SortCode
+            //               select m;
+            var children = self.Children.OrderBy(m => m.SortCode);
+            // 2.2关于子女循环，递归调用
+            foreach (var item in children)
+            {
+                AddSelfAndChildren(item, allMenu, allFunction, sb);
+            }
+            // 3.关闭自己
+            sb.Append("</item>");
         }
         #endregion
 
